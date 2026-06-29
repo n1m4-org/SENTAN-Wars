04e2327c0f5043006ba7f9d2c9a07fa3a7be5768
takuto-0130 <k023g0005@g.neec.ac.jp>
2026-06-29 11:13:37 +0900

    feat: BaseEnemyクラスの初期化とステータス追加

--- app/src/Character/Enemy/BaseEnemy.cpp
+++ app/src/Character/Enemy/BaseEnemy.cpp
@@ -1,1 +1,13 @@
 #include "BaseEnemy.h"
+#include <3d/Object/Base/BaseObjectManager.h>
+
+using namespace Hagine;
+
+void BaseEnemy::Init(const std::string className)
+{
+	BaseObject::Init(className);
+	// 仮モデル
+	CreatePrimitiveModel(PrimitiveType::Sphere);
+
+	UniqueInit();
+}
--- app/src/Character/Enemy/BaseEnemy.h
+++ app/src/Character/Enemy/BaseEnemy.h
@@ -1,4 +1,29 @@
 #pragma once
+#include <3d/Object/Base/BaseObject.h>
+
+struct EnemyStatus
+{
+	int cost = 0;
+	float hp = 0.0f;
+	float power = 0.0f;
+	float movementSpeed = 0.0f;
+};
+
 class BaseEnemy
-{};
+	: public Hagine::BaseObject
+{
+public:
+	void Init(const std::string className) override;
+
+
+protected:
+	/// <summary>
+	/// 固有初期化処理
+	/// </summary>
+	virtual void UniqueInit() {};
+
+
+private:
+	EnemyStatus status_;
+};
 
